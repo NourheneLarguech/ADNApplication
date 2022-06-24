dfa47c6634cb382e2bf92a4d5bc1fa22f2a4f092
Nourhene Larguech <nourhene.larguech@ensi-uma.tn>
2022-06-24 09:20:17 +0100

    second commit

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {VersionApplicableComponent} from "./version-applicable/list/version-applicable.component";
-import {UserRouteAccessService} from "../core/auth/user-route-access.service";
-import {EditComponent} from "./update/edit/edit.component";
-import {
-  VersionApplicableRoutingResolveService
-} from "./version-applicable/route/version-applicable-routing-resolve.service";
-import {UpdateModule} from "./update/update.module";
+import { VersionApplicableComponent } from './version-applicable/list/version-applicable.component';
+import { UserRouteAccessService } from '../core/auth/user-route-access.service';
+import { EditComponent } from './update/edit/edit.component';
+import { VersionApplicableRoutingResolveService } from './version-applicable/route/version-applicable-routing-resolve.service';
+import { UpdateModule } from './update/update.module';
 
 @NgModule({
   imports: [
@@ -37,6 +35,12 @@ import {UpdateModule} from "./update/update.module";
         path: 'version-cible',
         data: { pageTitle: 'adnApplicationApp.versionCible.home.title' },
         loadChildren: () => import('./version-cible/version-cible.module').then(m => m.VersionCibleModule),
+      },
+
+      {
+        path: 'client',
+        data: { pageTitle: 'adnApplicationApp.update.home.title' },
+        loadChildren: () => import('./client/client.module').then(m => m.ClientModule),
       },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
