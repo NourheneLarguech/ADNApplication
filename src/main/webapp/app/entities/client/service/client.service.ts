160bb833c0cb258526ab4522a4f1bb3add424276
Nourhene Larguech <nourhene.larguech@ensi-uma.tn>
2022-07-26 10:04:29 +0100

    commit 26/07/2022

--- src/main/webapp/app/entities/client/service/client.service.ts
+++ src/main/webapp/app/entities/client/service/client.service.ts
@@ -16,6 +16,7 @@ export class ClientService {
   profil: BehaviorSubject<any>;
   observable!: Observable<any>;
   protected resourceUrl = this.applicationConfigService.getEndpointFor('api/clients');
+  nameClient?: string;
 
   constructor(protected http: HttpClient, protected applicationConfigService: ApplicationConfigService) {
     this.profil = new BehaviorSubject<any>('');
@@ -77,6 +78,7 @@ export class ClientService {
       {
         versions: [],
         updates: [],
+
         id: '',
         uid: client?.client_product?.uidProduct,
         name: client?.client_product?.nameProduct,
@@ -153,12 +155,13 @@ export class ClientService {
 
   createV0(client: IClient, version: any): Observable<any> {
     const token: string | null = `${String(localStorage.getItem('authenticationToken'))}`;
+    const nameVersion = client?.productClient?.concat('_ActimuxIndexV0');
     console.log('token :' + token);
     return this.http.post<any>(
       'http://localhost:8080/api/clients',
       {
         uidClient: version.uid,
-        nameClient: client.nameClient,
+        nameClient: nameVersion,
         productClient: client.productClient,
         comment: client.comment,
         description: client.description,
@@ -174,15 +177,61 @@ export class ClientService {
       { headers: { authorization: `Bearer ${token}` } }
     );
   }
-  createVersionV0(client: IClient, todayString: string, uid: string, createby: string): Observable<any> {
+  createVersion2(client: IClient, newVersion?: string): Observable<any> {
+    const token: string | null = `${String(localStorage.getItem('PlatformAPIToken'))}`;
+
+    // let ver = client?.productClient?.substring(client.productClient?.length-1,client.productClient?.length-1)
+    // Number(ver);
+    // if(ver!==undefined){
+    // ver=ver+1;
+    // ver=ver.toString()
+    //  this.nameClient=client?.productClient?.concat('_ActimuxIndexV').concat(ver)
+    // console.log('token :' + this.nameClient);
+    // console.log(client?.productClient?.concat('_ActimuxIndexV').concat(ver));}
+
+    return this.http.post<any>(
+      'https://test.actiaadn.com/api/v1/version/',
+      { name: newVersion, comment: client.comment, description: client.description, productUid: client.client_product?.uidProduct },
+      { headers: { authorization: token } }
+    );
+  }
+  // createUpdateADN(client:IClient,uidApplicable:string,uidCible:string):Observable<any>{
+  //     console.log('createUpload');
+  //     const token: string | null= `${String(localStorage.getItem('PlatformAPIToken'))}`;
+  //     return this.http.post<any>('https://test.actiaadn.com/api/v1/update/',
+  //         {
+  //             "titles": {},
+  //             "descriptions": {},
+  //             "status": "SUSPENDED",
+  //             "productUid":client.client_product?.uidProduct,
+  //             "appliedVersions": [
+  //                 {
+  //                     "uid": uidApplicable,
+  //                     "name": client.versionApplicable,
+  //                     "comment": client.comment,
+  //                     "productUid":client.client_product?.uidProduct
+  //                 }
+  //             ],
+  //             "linkedVersion": {
+  //                 "uid": uidCible,
+  //                 "name": ,
+  //                 "comment": client.comment,
+  //                 "productUid": client.client_product?.uidProduct
+  //             },
+  //             "name": Update.versionName
+  //         },
+  //         {headers:{"authorization":token}})
+  //
+  // }
+  createVersionApplicable(client: IClient, todayString: string, createby: string): Observable<any> {
     const token: string | null = `${String(localStorage.getItem('authenticationToken'))}`;
     // console.log(todayString);
     console.log('token :' + token);
     return this.http.post<any>(
       'http://localhost:8080/api/version-applicables',
       {
-        uidVersionApplicable: uid,
-        nameVersionApplicable: client.versionApplicable?.nameVersionApplicable,
+        uidVersionApplicable: client.uidClient,
+        nameVersionApplicable: client.nameClient,
         comment: client.comment,
         description: client.description,
         createDate: todayString,
@@ -200,6 +249,136 @@ export class ClientService {
       { headers: { authorization: `Bearer ${token}` } }
     );
   }
+  createVersionCible(client: IClient, todayString: string, uid: string, name: string, createby: string): Observable<any> {
+    const token: string | null = `${String(localStorage.getItem('authenticationToken'))}`;
+    //console.log('token :'+ token)
+    return this.http.post<any>(
+      'http://localhost:8080/api/version-cibles',
+      {
+        uidVersionCible: uid,
+        nameVersionCible: name,
+        comment: client.comment,
+        description: client.description,
+        createDate: todayString,
+        modifyBy: createby,
+        modifidDate: todayString,
+        product: {
+          id: client.client_product?.id,
+          uidProduct: client.client_product?.uidProduct,
+          nameProduct: client.client_product?.nameProduct,
+          versionApplicables: null,
+          versionCibles: null,
+          updates: null,
+        },
+      },
+      { headers: { authorization: `Bearer ${token}` } }
+    );
+  }
+
+  /* Update à plateforme ADN */
+  createUpdateADN(
+    client: IClient,
+    uidApplicable?: string | null,
+    uidCible?: string | null,
+    nameVersionApplicable?: string | null,
+    nameVersionCible?: string | null,
+    name?: string | null
+  ): Observable<any> {
+    console.log('createUpload');
+    const token: string | null = `${String(localStorage.getItem('PlatformAPIToken'))}`;
+    return this.http.post<any>(
+      'https://test.actiaadn.com/api/v1/update/',
+      {
+        titles: {},
+        descriptions: {},
+        status: 'SUSPENDED',
+        productUid: client.client_product?.uidProduct,
+        appliedVersions: [
+          {
+            uid: uidApplicable,
+            name: nameVersionApplicable,
+            comment: client.comment,
+            productUid: client.client_product?.uidProduct,
+          },
+        ],
+        linkedVersion: {
+          uid: uidCible,
+          name: nameVersionCible,
+          comment: client.comment,
+          productUid: client.client_product?.uidProduct,
+        },
+        name: name,
+      },
+      { headers: { authorization: token } }
+    );
+  }
+  UpdateClient(
+    client: IClient,
+    idva: any,
+    idvc: any,
+    uid: string,
+    nameVersion?: string | null,
+    uidVersionApplicable?: string,
+    uidVersionCible?: string,
+    nameVersionApplicable?: string | null,
+    nameVersionCible?: string,
+    todayString?: string
+  ): Observable<any> {
+    const token: string | null = `${String(localStorage.getItem('authenticationToken'))}`;
+    console.log('token :' + token);
+    let id = client.id?.toString();
+    return this.http.put<any>(
+      `http://localhost:8080/api/clients/${id}`,
+      {
+        id: id,
+        uidClient: uid,
+        nameClient: nameVersion,
+        productClient: client.productClient,
+        comment: client.comment,
+        description: client.description,
+        statut: 'SUSPENDED',
+        client_product: {
+          id: client.client_product?.id,
+          uidProduct: client.client_product?.uidProduct,
+          nameProduct: client.client_product?.nameProduct,
+        },
+        versionApplicable: {
+          id: idva,
+          uidVersionApplicable: uidVersionApplicable,
+          nameVersionApplicable: nameVersionApplicable,
+          comment: client.comment,
+          description: client.description,
+          createDate: todayString,
+          modifyBy: todayString,
+          modifidDate: todayString,
+          updates: null,
+          product: {
+            id: client.client_product?.id,
+            uidProduct: client.client_product?.uidProduct,
+            nameProduct: client.client_product?.nameProduct,
+          },
+        },
+        versionCible: {
+          id: idvc,
+          uidVersionCible: uidVersionCible,
+          nameVersionCible: nameVersionCible,
+          comment: client.comment,
+          description: client.description,
+          createDate: todayString,
+          modifyBy: todayString,
+          modifidDate: todayString,
+          updates: null,
+          product: {
+            id: client.client_product?.id,
+            uidProduct: client.client_product?.uidProduct,
+            nameProduct: client.client_product?.nameProduct,
+          },
+        },
+      },
+      { headers: { authorization: `Bearer ${token}` } }
+    );
+  }
+
   viewDroitClient(): Observable<any> {
     const token: string | null = `${String(localStorage.getItem('authenticationToken'))}`;
     return this.http.get<any>('http://localhost:8080/api/client-updates?page=0&size=20&sort=id,asc', {
